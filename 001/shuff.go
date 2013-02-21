34c4b0b14b20e920c6d08bde408c761b24852d6e
wangjinlei <wangjinlei4ustc@gmail.com>
2013-02-21 09:46:48 +0800

    finished 001 golang version

--- 001/shuff.go
+++ 001/shuff.go
@@ -5,20 +5,55 @@
 ///////////////////////////////////////////////////////
 /*
  * Problem Description
- * 给出洗牌的一个算法，并将洗好的牌存储在一个整形数组里。
+ * 缁欏嚭娲楃墝鐨勪竴涓畻娉曪紝骞跺皢娲楀ソ鐨勭墝瀛樺偍鍦ㄤ竴涓暣褰㈡暟缁勯噷
  */
-
-package main 
+package main
 
 import (
-	"fmt"
+    "fmt"
+    "math/rand"
+    "sort"
+    "time"
 )
 
+// fill the slice with sequence integer from zero to len(data)-1
+func fill(data []int) {
+
+    for i := 0; i < len(data); i++ {
+        data[i] = i
+    }
+}
+
+func swap(data []int, i, j int) {
+
+    data[i], data[j] = data[j], data[i]
+}
+
+// generate a random num within [from, to)
+func randRange(from, to int) (r int) {
+
+    source := rand.NewSource(time.Now().UnixNano())
+    rnd := rand.New(source)
+    span := to - from
+    rndInt = int(rnd.Int31())%span + from
+    return rndInt
+}
+
 func shuffle(data []int) {
-	
+
+    for i := 0; i < len(data); i++ {
+        r := randRange(i, len(data)) // Notice here, not range[0, n)
+        swap(data, i, r)
+    }
 }
 
 func main() {
-	
-}
 
+    data := make([]int, 100)
+    fill(data)
+    shuffle(data)
+    fmt.Println(data)
+
+    sort.Ints(data)
+    fmt.Println(data)
+}
